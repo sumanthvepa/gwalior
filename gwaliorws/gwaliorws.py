130acf3a0f44b154791aa78077f0f355e30e6462
Sumanth Vepa <svepa@milestone42.com>
2022-11-09 12:34:14 +0530

    [issue002] Implement correct skeleton responses for all endpoints
    FEATURE(s):
    issue002-skeleton: Implement a skeleton app with all routes.
    
    FILES(s):
    [modified]TODO.txt:
      Mark issue02 as done.
    [modified]gwaliorws/gwaliorws.py:
      Implement correct skeleton responses for all endpoints
    
    DESCRIPTION
    The web service now returns properly formed JSON responses
    for all endpoints. This is the final API for the version 0
    release of the gwalior.

--- gwaliorws/gwaliorws.py
+++ gwaliorws/gwaliorws.py
@@ -16,29 +16,76 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+import json
 from flask import Flask, Response
+from markupsafe import escape
 
 app = Flask(__name__)
+
+_services = {
+    'test1': {
+        'status': 'stopped'
+    },
+    'test2': {
+        'status': 'stopped'
+    },
+    'flaky': {
+        'status': 'stopped'
+    }
+}
 
 
 @app.route('/services')
 def services():
     return [
-        {'name': 'test1'},
-        {'name': 'test'}
+        {'service_name': service_name}
+        for service_name in _services.keys()
     ]
 
 
 @app.route('/<string:service_name>/status')
 def status(service_name):
-    return {'status': 'stopped'}
+    service_name = escape(service_name)
+    if service_name in _services.keys():
+        return {
+            'status': _services.get(service_name).get('status')
+        }
+    return '', 404
 
 
 @app.route('/<string:service_name>/start')
 def start(service_name):
-    return '', 200
+    service_name = escape(service_name)
+    if service_name in _services.keys():
+        if service_name == 'flaky':
+            return Response(
+                response=json.dumps({
+                    'error_code': 1,
+                    'error_message': 'Unable to start the web service',
+                    'error_detail': 'It is dummy service that will '
+                                    + 'fail to start by design'
+                }),
+                status=500,
+                mimetype='application/json')
+        _services[service_name]['status'] = 'running'
+        return '', 200
+    return '', 404
 
 
 @app.route('/<string:service_name>/stop')
 def stop(service_name):
-    return '', 200
+    service_name = escape(service_name)
+    if service_name in _services.keys():
+        if service_name == 'flaky':
+            return Response(
+                response=json.dumps({
+                    'error_code': 1,
+                    'error_message': 'Unable to stop the web service',
+                    'error_detail': 'It is dummy service that will '
+                                    + 'fail to stop by design'
+                }),
+                status=500,
+                mimetype='application/json')
+        _services[service_name]['status'] = 'stopped'
+        return '', 200
+    return '', 404
